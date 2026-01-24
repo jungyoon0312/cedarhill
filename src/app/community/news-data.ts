017a54477e42b0815aecd16f6e068ee9bd74d5b9
JUNGYOON YOO <jungyoon0312@gmail.com>
2026-01-24 23:30:20 +0900

    Add second admission briefing news post (2026-01-27)

--- src/app/community/news-data.ts
+++ src/app/community/news-data.ts
@@ -25,5 +25,27 @@ export const newsPosts: NewsPost[] = [
       "/images/IMG_5291.jpeg",
     ],
   },
+  {
+    id: "admission-briefing-2026-01-27",
+    title: "2회 입학설명회가 진행되었습니다",
+    date: "2026-01-27",
+    content: "2026년 1월 27일(토), 인테리어 공사가 마무리중인 씨더힐 현장에서 2차 입학 설명회가 진행되었습니다.\n\n굳은 날씨에도 많은 가족들이 참석해 주셨습니다.\n\n약 한시간의 학교소개와 커리큘럼 안내 이후, 마무리 단계에 있는 학교 시설을 돌아보는 간단한 투어가 가능한 날이었습니다.\n\n씨더힐 1회 입학생들은 새로운 시설을 처음으로 이용하게 됩니다. 기대해주세요!",
+    images: [
+      "/images/KakaoTalk_20260124_154425265_01.jpg",
+      "/images/KakaoTalk_20260124_155143946_01.jpg",
+      "/images/KakaoTalk_20260124_155143946_02.jpg",
+      "/images/KakaoTalk_20260124_155143946_09.jpg",
+      "/images/KakaoTalk_20260124_155143946_22.jpg",
+      "/images/KakaoTalk_20260124_155143946_24.jpg",
+      "/images/KakaoTalk_20260124_155143946_26.jpg",
+      "/images/KakaoTalk_20260124_155143946_27.jpg",
+      "/images/KakaoTalk_20260124_155143946_28.jpg",
+      "/images/KakaoTalk_20260124_155225906_08.jpg",
+      "/images/KakaoTalk_20260124_155225906_16.jpg",
+      "/images/KakaoTalk_20260124_155225906_19.jpg",
+      "/images/KakaoTalk_20260124_155225906_21.jpg",
+      "/images/KakaoTalk_20260124_155225906_22.jpg",
+    ],
+  },
 ];
 
